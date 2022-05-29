dd0a2ee1d3a8729655e3f8c4fd27956fbe5c2d06
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2022-05-29 18:10:59 -0300

    Atividade_1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite a idade do aluno mais novo:");
+int idademin = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a idade do aluno mais velho:");
+int idademax = Convert.ToInt32(Console.ReadLine());
+double idademedia = 0.0;
+
+idademedia = (idademin + idademax) / 2;
+
+Console.WriteLine($"A idade média é:  {idademedia}");
